87303e9facf203581dbfc2d2a5089eb55c622198
Ben Wong <benjaminwg1028@gmail.com>
2023-07-03 16:39:26 +0800

    Created a prototype of search page

--- src/App.js
+++ src/App.js
@@ -4,28 +4,10 @@ import { useState, React } from 'react';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
-
-// function SearchBar() {
-//   return (
-//     <MDBCol md="6">
-//       <form className="form-inline mt-4 mb-4">
-//         <MDBIcon icon="search" />
-//         <input className="form-control form-control-sm ml-3 w-75" type="text" placeholder="Search" aria-label="Search" />
-//       </form>
-//     </MDBCol>
-//   );
-// }
+import Img from './assets/6-380-002.jpg';
 
 function App() {
-  // const [items, setItems] = useState([]);
-
-  //     set search query to empty string
   const [query, setQuery] = useState("");
-  //     set search parameters
-  //     we only what to search countries by capital and name
-  //     this list can be longer if you want
-  //     you can search countries even by their population
-  // just add it to this array
   const [searchParam] = useState(["name"]);
 
   // Use a database in the future
@@ -37,57 +19,65 @@ function App() {
     {
       "name": "Electric Tow Truck",
       "price": 500
+    },
+    {
+      "name": "Vertical Milling Machine 2",
+      "price": 700
+    },
+    {
+      "name": "Electric Tow Truck 2",
+      "price": 500
+    },
+    {
+      "name": "Electric Tow Truck 2",
+      "price": 500
+    },
+    {
+      "name": "Electric Tow Truck 2",
+      "price": 500
     }
   ];
 
   return (
     <div className="App">
-      <div className="wrapper">
-        <div className="search-wrapper">
-            <label htmlFor="search-form">
-              <input
-                  type="search"
-                  name="search-form"
-                  id="search-form"
-                  className="search-input"
-                  placeholder="Search for..."
-                  value={query}
-                  onChange={(e) => setQuery(e.target.value)}
-              />
-              <span className="sr-only">Search countries here</span>
-            </label>
-        </div>
-        <div className="card-grid">
-          {items.filter((item) => {
-            return searchParam.some((newItem) => {
-              return (
-                  item[newItem]
-                      .toString()
-                      .toLowerCase()
-                      .indexOf(query.toLowerCase()) > -1
-              );
-            });
-          }).map((item) => (
-            // <li>
-            <article className="card" key={item.callingCodes}>
-              {/* <div className="card-image">
-                  <img src={item.flag} alt={item.name} />
-              </div> */}
-              <div className="card-content">
-                <h2 className="card-name">{item.name}</h2>
-                <ol className="card-list">
-                  <li>
-                      Name: <span>{item.name}</span>
-                  </li>
-                  <li>
-                      Price: <span>{"$" + item.price}</span>
-                  </li>
-                </ol>
+      <div className="search-wrapper">
+        <label htmlFor="search-form">
+          <input
+              type="search"
+              name="search-form"
+              id="search-form"
+              className="search-input"
+              placeholder="Search for..."
+              value={query}
+              onChange={(e) => setQuery(e.target.value)}
+          />
+        </label>
+      </div>
+      <div className="row">
+        {items.filter((item) => {
+          return searchParam.some((newItem) => {
+            return (
+                item[newItem]
+                    .toString()
+                    .toLowerCase()
+                    .indexOf(query.toLowerCase()) > -1
+            );
+          });
+        }).map((item) => (
+          <div class="col-sm-3 pt-5">
+            <div class="card text-left">
+              <img class="card-img-top h-50 w-100" src={Img} alt="Card image cap"/>
+              <div class="card-body">
+                <h5 class="card-title">{item.name}</h5>
+                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                <p class="card-text">Price: {item.price}</p>
               </div>
-            </article>
-            // </li>
-          ))}
-        </div>
+              <div class="card">
+                <a href="#" class="btn btn-primary stretched-link">Link</a>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
