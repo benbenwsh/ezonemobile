6f51507aab535399e36e40a5a4f2b53743b593f5
Ben Wong <benjaminwg1028@gmail.com>
2023-07-05 11:26:10 +0800

    Revert "Connected Node.js to MySQL (but commented out cuz only work with local MySQL server)"
    This reverts commit 101fa06df4136fc73ab0f7f35d24ba5d98d6499b.

--- backend/index.js
+++ backend/index.js
@@ -1,29 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const mysql = require('mysql');
-const cors = require('cors');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'fotama',
-  database: 'test',
+app.use(express.static('dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
-app.use(cors());
-
-app.get('/api/data', (req, res) => {
-  connection.query("SELECT * FROM item", (error, result) => {
-    if (error) {
-      console.error('Error executing SELECT:', error);
-      res.status(500).json({ error: 'Internal server error' });
-    } else {
-      res.json(result);
-    }
-  });
-});
-
-app.listen(3001, () => {
-  console.log('Server is running on http://localhost:3001');
+app.listen(3000, () => {
+  console.log('Server is running on http://localhost:3000');
 });
